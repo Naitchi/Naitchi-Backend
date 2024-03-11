3b6404cb47fdac6f20ff1351da5850550cebf36c
Naitchi <assasindu39@gmail.com>
2024-03-11 18:37:46 +0100

    ⚰️ deleting useless code in app

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 import express from 'express';
 import http from 'http';
 import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url'; // Importer la fonction fileURLToPath depuis le module 'url'
-
-const __filename = fileURLToPath(import.meta.url); // Obtenir le chemin du fichier en cours d'exécution
-const __dirname = path.dirname(__filename); // Obtenir le répertoire du fichier en cours d'exécution
 
 const app = express();
 const server = http.createServer(app);
@@ -18,14 +13,9 @@ dotenv.config();
 // Nécessaire pour le router
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/public/index.html`);
-});
-
 app.use('/api/', index);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
   console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
